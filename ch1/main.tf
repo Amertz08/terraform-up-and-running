d1a49bccf2c616cc751fd57d9a0c3c0015c72647
Adam Mertz <adammertz@gmail.com>
2019-02-06 12:51:39 -0600

    finished ch2

--- ch1/main.tf
+++ ch1/main.tf
@@ -3,11 +3,100 @@ provider "aws" {
     profile = "personal"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-0cd3dfa4e37921605"
-    instance_type = "t2.micro"
+data "aws_availability_zones" "all" {}
 
-    tags {
-        Name = "terraform-example"
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = "${aws_launch_configuration.example.id}"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+    load_balancers = ["${aws_elb.example.name}"]
+    health_check_type = "ELB"
+
+    min_size = 2
+    max_size = 10
+
+    tag  {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
     }
+}
+
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0f65671a86f061fcd"
+    instance_type = "t2.micro"
+    security_groups = ["${aws_security_group.instance.id}"]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p "${var.server_port}" &
+                EOF
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+      from_port = "${var.server_port}"
+      to_port = "${var.server_port}"
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_elb" "example" {
+    name = "terraform-asg-example"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    security_groups = ["${aws_security_group.elb.id}"]
+
+    listener {
+      instance_port = 80
+      instance_protocol = "http"
+      lb_port = "${var.server_port}"
+      lb_protocol = "http"
+    }
+
+    health_check {
+      healthy_threshold = 2
+      unhealthy_threshold = 2
+      timeout = 3
+      target = "HTTP:${var.server_port}/"
+      interval = 30
+    }
+}
+
+resource "aws_security_group" "elb" {
+    name = "terraform-example-elb"
+
+    ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+output "elb_dns_name" {
+    value = "${aws_elb.example.dns_name}"
 }
